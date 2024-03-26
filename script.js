520a61c4e179891639973297c7f47024e5f30df6
Pak <pakapongm@gmail.com>
2024-03-25 18:04:39 -0700

    prompt the dev a question

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-require('dotenv').config();
+// dependencies
 const { OpenAI } = require('langchain/llms/openai');
 const inquirer = require('inquirer');
+require('dotenv').config();
+
+// Creates and stores a wrapper for the OpenAI package along with basic configuration
 const model = new OpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    // represents the temperature of the model, higher values will generate more creative responses
-    // ranges from 0-1, default is 0.5, 0 meaning the model will be deterministic and 1 meaning the model will be very creative 
-    temperature: 0,
-    model: 'gpt-3.5-turbo'
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  // temperature defaults to 0.5
+  // set to 0 for deterministic results
+  // set to 1 for more randomness
+  // range is 0-1
+  temperature: 0,
+  model: 'gpt-3.5-turbo'
 });
 
-console.log({ model });
+console.log({model});
 
+// Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
 const promptFunc = async (input) => {
-    try {
-        const res = await model.call(input);
-        console.log(res);
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    const res = await model.call(input);
+    console.log(res);
+  }
+  catch (err) {
+    console.error(err);
+  }
 };
-promptFunc();
 
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
 const init = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Ask a coding question:',
-        },
-    ]).then((inquirerResponse) => {
-        promptFunc(inquirerResponse.name)
-    });
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Ask a coding question:',
+    },
+  ]).then((inquirerResponse) => {
+    promptFunc(inquirerResponse.name)
+  });
 };
+
+// Calls the initialization function and starts the script
 init();
