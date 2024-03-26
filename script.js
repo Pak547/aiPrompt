84ffdf94009635d3036db192a7bb1891b9304d60
Pak <pakapongm@gmail.com>
2024-03-25 18:02:09 -0700

    made it more dynamic by adding inquirer

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 require('dotenv').config();
 const { OpenAI } = require('langchain/llms/openai');
-
-const model = new OpenAI({ 
+const inquirer = require('inquirer');
+const model = new OpenAI({
     openAIApiKey: process.env.OPENAI_API_KEY,
     // represents the temperature of the model, higher values will generate more creative responses
     // ranges from 0-1, default is 0.5, 0 meaning the model will be deterministic and 1 meaning the model will be very creative 
     temperature: 0,
     model: 'gpt-3.5-turbo'
-  });
-  
-  console.log({ model });
+});
 
-  const promptFunc = async () => {
+console.log({ model });
+
+const promptFunc = async (input) => {
     try {
-        const res = await model.call("How do you capitalize all characters of a string in JavaScript?");
+        const res = await model.call(input);
         console.log(res);
-      }
-      catch (err) {
+    }
+    catch (err) {
         console.error(err);
-      }
-  };
-  
-  promptFunc();
+    }
+};
+promptFunc();
+
+const init = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Ask a coding question:',
+        },
+    ]).then((inquirerResponse) => {
+        promptFunc(inquirerResponse.name)
+    });
+};
+init();
